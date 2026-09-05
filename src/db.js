e9aa75eb97b2bb47958eb59594838b9ae5c09efd
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2026-09-05 15:52:08 +0500

    d

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const Database = require('better-sqlite3');
-
+console.log('test')
 const dataDir = path.join(__dirname, '..', 'data');
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
 
